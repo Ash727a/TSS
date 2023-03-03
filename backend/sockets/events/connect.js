b2a40cd785e1a56e771db33b2b821d987219264b
Dilraj Gill <dilgill1234@gmail.com>
2023-03-03 17:26:11 -0600

    Add initial files from TSS_Hold

---
+++ backend/sockets/events/connect.js
@@ -0,0 +1,41 @@
+class User {
+    constructor(username, client_id, room_id) {
+        this.uRoomId = room_id;
+        this.uUsername = username;
+        this.uClientId = client_id;
+    }
+
+    async registerUser(reg, models) {
+        const user = await models.user;
+        const room = await models.room;
+
+        try {
+            const existingUser = await user.findOne({ where: { client_id: this.uClientId } });
+            const assigned_room = await room.findOne({ where: { id: this.uRoomId, client_id: null } });
+            //check if user is already registered
+            if (!existingUser) {
+                console.log(reg);
+                await user.create(reg); //register the new user
+                console.log(`${this.uUsername} successfully registered.`);
+            }
+            else
+                console.log(`${this.uUsername} is already registered.`);
+
+            //check if assigned room is vacant
+            if (assigned_room) {
+                await room.update({ client_id: this.uClientId }, { where: { id: this.uRoomId } }); //assign the client_id to the room
+                await assigned_room.save();
+                console.log(`${this.uUsername} assigned to room ${this.uRoomId}.`);
+                return false;
+            }
+            else {
+                console.log(`This room is not available.`); //this shouldn't happen
+                return true;
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+}
+
+module.exports = User;
