d2e48ed7a8e05860f5abcb7dded8d706ee3bda09
Josh Wong <joshwong74@gmail.com>
2023-03-06 17:09:17 -0600

    Added typescript import sorter support

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,15 @@
 require('dotenv').config();
-// const express = require("express");
-// const cors = require('cors');
 const sequelize = require('./app/database');
 const app = require('./app/server/express/app');
-// const app = express();
 
 // Environment variables
 const API_URL = process.env.API_URL;
 const API_PORT = process.env.API_PORT;
 const SOCKET_PORT = process.env.SOCKET_PORT;
 
-// const corsOptions = {
-//   origin: `http://${API_URL}:${API_PORT}`,
-// };
-
 // Log the environment variables
 console.log(`API PORT: ${API_PORT}`);
 console.log(`SOCKET PORT: ${SOCKET_PORT}`);
-
-// app.use(cors(corsOptions));
-
-// Parse requests of content-type - application/json
-// app.use(express.json());
-
-// Parse requests of content-type - application/x-www-form-urlencoded
-// app.use(express.urlencoded({ extended: true }));
 
 async function assertDatabaseConnectionOk() {
   console.log(`Checking database connection...`);
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,37 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { UIAComponent } from './modules/uia/uia.component';
-import { TelemetryComponent } from './modules/telemetry/telemetry.component';
-import { RoomsComponent } from './modules/rooms/rooms.component';
+
 import { LogsComponent } from './modules/logs/logs.component';
+import { RoomsComponent } from './modules/rooms/rooms.component';
+import { TelemetryComponent } from './modules/telemetry/telemetry.component';
+import { UIAComponent } from './modules/uia/uia.component';
 
 const routes: Routes = [
   {
-      path: 'rooms',
-      component: RoomsComponent
+    path: 'rooms',
+    component: RoomsComponent,
   },
   {
-      path: 'uia',
-      component: UIAComponent
+    path: 'uia',
+    component: UIAComponent,
   },
   {
-      path: 'telemetry',
-      component: TelemetryComponent
+    path: 'telemetry',
+    component: TelemetryComponent,
   },
   {
-
-      path: 'logs',
-      component: LogsComponent
+    path: 'logs',
+    component: LogsComponent,
   },
   {
-      path: '',
-      redirectTo: '',
-      pathMatch: 'full'
-  }
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
