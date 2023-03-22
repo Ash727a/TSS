446bb5f3693c5dd4b1d765502084d8d17f45a938
Dilraj Gill <dilgill1234@gmail.com>
2023-03-22 15:16:39 -0500

    Finish renaming files for User class

---
+++ backend/src/server/sockets/events/user.class.ts
@@ -0,0 +1,51 @@
+import { primaryKeyOf } from '../../../helpers.js';
+import { CrewmemberMsgBlob } from '../socket_interfaces.js';
+import { SequelizeModel } from '../../../interfaces.js';
+import { ILiveModels } from '../../../database/models/index.js';
+// import { ISocketServerModels } from '../model_interfaces.js';
+
+class User {
+  uRoomId: number;
+  uUsername: string;
+  uClientId: string;
+  constructor(username: string, client_id: string, room_id: number) {
+    this.uRoomId = room_id;
+    this.uUsername = username;
+    this.uClientId = client_id;
+  }
+
+  async registerUser(
+    registration_data: CrewmemberMsgBlob['DATA'],
+    models: Pick<ILiveModels, 'user' | 'room'>
+  ): Promise<boolean> {
+    const user = await models.user;
+    const room = await models.room;
+
+    try {
+      const existingUser = await user.findOne({ where: { [primaryKeyOf(user)]: this.uClientId } });
+      const assigned_room = await room.findOne({ where: { [primaryKeyOf(room)]: this.uRoomId, client_id: null } });
+      //check if user is already registered
+      if (!existingUser) {
+        console.log(registration_data);
+        await user.create(registration_data); //register the new user
+        console.log(`${this.uUsername} successfully registered.`);
+      } else console.log(`${this.uUsername} is already registered.`);
+
+      //check if assigned room is vacant
+      if (assigned_room) {
+        await room.update({ client_id: this.uClientId }, { where: { [primaryKeyOf(room)]: this.uRoomId } }); //assign the client_id to the room
+        await assigned_room.save();
+        console.log(`${this.uUsername} assigned to room ${this.uRoomId}.`);
+        return false;
+      } else {
+        console.log(`This room is not available.`); //this shouldn't happen
+        return true;
+      }
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+  }
+}
+
+export default User;
