f392bbcbfded48801ada8c5c5228a92859dea02d
Dilraj Gill <dilgill1234@gmail.com>
2023-05-16 13:29:54 -0500

    Reconnect to rover + heartbeat

--- backend/src/server/sockets/rover/RoverSocketServer.ts
+++ backend/src/server/sockets/rover/RoverSocketServer.ts
@@ -2,12 +2,15 @@ import { IAllModels } from '../../../database/models/index.js';
 import type { IRosTypeSensorMsgsNavSatFix } from './roverInterfaces';
 import ROSLIB from 'roslib';
 
-const SOCKET_URL = 'ws://192.168.0.105:9090';
-const WHERE_CONSTRAINT = { where: { name: 'rover' } };
+// const SOCKET_URL = 'ws://192.168.0.105:9090';
+const SOCKET_URL = 'ws://leo-raspi.local:9090';
+
+const WHERE_CONSTRAINT = { where: { name: 'rover' } } as const;
 
 export class RoverSocketServer {
   private readonly _ros: ROSLIB.Ros;
   private readonly models: IAllModels;
+  private hbInterval: ReturnType<typeof setTimeout> | undefined = undefined;
 
   constructor(_models: IAllModels) {
     this.models = _models;
@@ -20,38 +23,60 @@ export class RoverSocketServer {
     // Set up socket event listeners
     this.initializeSocketListeners();
 
-    const cmdVel = new ROSLIB.Topic({
-      ros: this._ros,
-      name: '/cmd_vel',
-      messageType: 'geometry_msgs/Twist',
-    });
+    // const cmdVel = new ROSLIB.Topic({
+    //   ros: this._ros,
+    //   name: '/cmd_vel',
+    //   messageType: 'geometry_msgs/Twist',
+    // });
 
-    const twist = new ROSLIB.Message({
-      lat: 0,
-      lon: 0,
-    });
-    console.log('Publishing cmd_vel');
-    cmdVel.publish(twist);
+    // const twist = new ROSLIB.Message({
+    //   lat: 0,
+    //   lon: 0,
+    // });
+    // console.log('Publishing cmd_vel');
+    // cmdVel.publish(twist);
   }
 
   private initializeSocketListeners(): void {
     this._ros.on('connection', () => {
-      console.log('Connected to websocket server.');
+      console.log('Connected to ROVER websocket server.');
       this.models.devices.update({ is_connected: true, connected_at: new Date() }, WHERE_CONSTRAINT);
+
+      this.hbInterval = setInterval(() => {
+        this.sendEmptyMessage();
+      }, 1000);
     });
 
     this._ros.on('error', (error) => {
-      console.log('Error connecting to websocket server: ', error);
+      console.log('Error connecting to ROVER websocket server: ', error);
       this.models.devices.update({ is_connected: false }, WHERE_CONSTRAINT);
+      this.connect();
     });
 
     this._ros.on('close', () => {
-      console.log('Connection to websocket server closed.');
+      console.log('Connection to ROVER websocket server closed.');
       this.models.devices.update({ is_connected: false }, WHERE_CONSTRAINT);
+      clearInterval(this.hbInterval);
+      this.connect();
     });
+  }
+
+  private sendEmptyMessage(): void {
+    const heartbeatTopic = new ROSLIB.Topic({
+      ros: this._ros,
+      name: '/stop_movement',
+      messageType: 'std_msgs/Empty',
+    });
+    // Send empty message as a heartbeat to rover
+    const heartbeatMessage = new ROSLIB.Message({});
+    heartbeatTopic.publish(heartbeatMessage);
   }
 
   public isConnected(): boolean {
     return this._ros.isConnected;
+  }
+
+  public connect(): void {
+    this._ros.connect(SOCKET_URL);
   }
 }
