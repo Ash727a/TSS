fa26993a7c6e84a52a73ee1df0b8a8c6990dadaf
Dilraj Gill <dilgill1234@gmail.com>
2023-05-18 23:31:12 -0500

    Initial  UIA Sim class

---
+++ backend/src/simulations/telemetry/UIA_Values.ts
@@ -0,0 +1,14 @@
+export enum SUPPLY_PRESSURE {
+  PRESSURIZATION_RATE = 10, // psi/sec
+  DEPRESSURIZATION_RATE = 10, // psi/sec
+}
+
+export enum AIRLOCK_PRESSURE {
+  PRESSURIZATION_RATE = 1, // psi/sec
+  DEPRESSURIZATION_RATE = 1, // psi/sec
+}
+
+export enum WATER {
+  FILL_RATE = 10, //   %/sec
+  EMPTY_RATE = 10, //   %/sec
+}
