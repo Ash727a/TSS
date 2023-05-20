02de7e1fdfb3c12736816d0d4c3e33745f8a3a90
Josh Wong <joshwong74@gmail.com>
2023-05-20 17:55:13 -0500

    Added test mode for UIA to simulate w/o UIA

--- frontend/src/app/modules/uia/uia.component.ts
+++ frontend/src/app/modules/uia/uia.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Room, StatusSensor, UIAData } from '@app/core/interfaces';
+import config from '@app/config';
 // Backend
 import { RoomsService } from '@services/api/rooms.service';
 import { UIAService } from '@services/api/uia.service';
@@ -15,6 +16,7 @@ export class UIAComponent {
   @Input() public variant: 'default' | 'small' = 'default';
   @Input() selectedRoom: Room | null = null;
 
+  protected TEST_MODE = config.TEST_MODE;
   protected sensors1: StatusSensor[] = [];
   protected sensors2: StatusSensor[] = [];
   private uiaData: UIAData = {} as UIAData;
@@ -41,32 +43,36 @@ export class UIAComponent {
       this.uiaService.getUIAStateByRoomID(roomID).then((result) => {
         if (result.ok) {
           this.uiaData = result.data;
-          this.connected = Boolean(this.uiaData?.started_at);
-          // Check time since the last update time, and if greater than 5 seconds, set connected to false in frontend and database
-          if (this.connected && this.uiaData?.updatedAt) {
-            const lastUpdate = new Date(this.uiaData.updatedAt);
-            const now = new Date();
-            const diff = now.getTime() - lastUpdate.getTime();
-            if (diff > 5000) {
-              this.connected = false;
-              // Set started_at to null in database. This will mark the UIA as not connected to the room
-              this.uiaService.updateByRoomID(roomID, { ...result.data, started_at: null })
+          if (!this.TEST_MODE) {
+            this.connected = Boolean(this.uiaData?.started_at);
+            // Check time since the last update time, and if greater than 5 seconds, set connected to false in frontend and database
+            if (this.connected && this.uiaData?.updatedAt) {
+              const lastUpdate = new Date(this.uiaData.updatedAt);
+              const now = new Date();
+              const diff = now.getTime() - lastUpdate.getTime();
+              if (diff > 5000) {
+                this.connected = false;
+                // Set started_at to null in database. This will mark the UIA as not connected to the room
+                this.uiaService.updateByRoomID(roomID, { ...result.data, started_at: null })
+              }
             }
+          } else {
+            this.connected = true;
           }
           this.sensors1 = [
-            { name: 'EMU1 POWER', status: this.setSwitchStatus(this.uiaData?.emu1_pwr_switch) },
-            { name: 'EV1 SUPPLY', status: this.setSwitchStatus(this.uiaData?.ev1_supply_switch) },
-            { name: 'EV1 WASTE', status: this.setSwitchStatus(this.uiaData?.ev1_water_waste_switch) },
-            { name: 'EV1 OXYGEN', status: this.setSwitchStatus(this.uiaData?.emu1_o2_supply_switch) },
-            { name: 'O2 VENT', status: this.setSwitchStatus(this.uiaData?.o2_vent_switch) },
+            { name: 'EMU1 POWER', status: this.setSwitchStatus(this.uiaData?.emu1_pwr_switch), key: 'emu1_pwr_switch' },
+            { name: 'EV1 SUPPLY', status: this.setSwitchStatus(this.uiaData?.ev1_supply_switch), key: 'ev1_supply_switch' },
+            { name: 'EV1 WASTE', status: this.setSwitchStatus(this.uiaData?.ev1_water_waste_switch), key: 'ev1_water_waste_switch' },
+            { name: 'EV1 OXYGEN', status: this.setSwitchStatus(this.uiaData?.emu1_o2_supply_switch), key: 'emu1_o2_supply_switch' },
+            { name: 'O2 VENT', status: this.setSwitchStatus(this.uiaData?.o2_vent_switch), key: 'o2_vent_switch' },
           ];
 
           this.sensors2 = [
-            { name: 'EMU2 POWER', status: this.setSwitchStatus(this.uiaData?.emu2_pwr_switch) },
-            { name: 'EV2 SUPPLY', status: this.setSwitchStatus(this.uiaData?.ev2_supply_switch) },
-            { name: 'EV2 WASTE', status: this.setSwitchStatus(this.uiaData?.ev2_water_waste_switch) },
-            { name: 'EV2 OXYGEN', status: this.setSwitchStatus(this.uiaData?.emu2_o2_supply_switch) },
-            { name: 'DEPRESS PUMP', status: this.setSwitchStatus(this.uiaData?.depress_pump_switch) },
+            { name: 'EMU2 POWER', status: this.setSwitchStatus(this.uiaData?.emu2_pwr_switch), key: 'emu2_pwr_switch' },
+            { name: 'EV2 SUPPLY', status: this.setSwitchStatus(this.uiaData?.ev2_supply_switch), key: 'ev2_supply_switch' },
+            { name: 'EV2 WASTE', status: this.setSwitchStatus(this.uiaData?.ev2_water_waste_switch), key: 'ev2_water_waste_switch' },
+            { name: 'EV2 OXYGEN', status: this.setSwitchStatus(this.uiaData?.emu2_o2_supply_switch), key: 'emu2_o2_supply_switch' },
+            { name: 'DEPRESS PUMP', status: this.setSwitchStatus(this.uiaData?.depress_pump_switch), key: 'depress_pump_switch' },
           ];
 
           // Get UIA telemetry data
@@ -95,5 +101,16 @@ export class UIAComponent {
     if (type === 'close' && room) {
       this.selectedRoom = room;
     }
+  }
+
+  protected toggleUIASwitch(sensor: any) {
+    const payload = {
+      ...this.uiaData,
+      [sensor.key]: !sensor.status,
+    }
+    if (this.selectedRoom?.id === undefined) {
+      return;
+    }
+    this.uiaService.updateByRoomID(this.selectedRoom.id, payload);
   }
 }
